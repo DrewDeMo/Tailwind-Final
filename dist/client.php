615cb5d023c189ecf560e4f7879688fb7ba16c1a
DrewDeMo <drdemaiolo@gmail.com>
2023-10-05 19:42:43 -0400

    Client Page fixed

--- dist/client.php
+++ dist/client.php
@@ -6,14 +6,33 @@
     <?php include 'head_links.php'; ?>
     <!--End heading links-->
 </head>
+<style>
+.tab-content {
+    opacity: 0;
+    height: 0;
+    overflow: hidden;
+    padding: 0;
+    transition: opacity 0.5s ease, height 0.5s ease, padding 0.5s ease;
+}
+
+.tab-content.active {
+    opacity: 1;
+    height: auto;
+    padding: 15px;
+    /* This can be adjusted based on your preference or set to 0 if unnecessary */
+}
+</style>
+
 
 <body>
     <div class="flex flex-wrap overflow-hidden bg-gray-100">
         <div class="w-full bg-gray-100 pl-0 lg:pl-64 min-h-screen mb-12 ">
-            <div id="main-nav" class="w-1/2 md:w-1/3 lg:w-64 fixed md:top-0 md:left-0 h-screen lg:block bg-white border-r z-30 hidden">
+            <div id="main-nav"
+                class="w-1/2 md:w-1/3 lg:w-64 fixed md:top-0 md:left-0 h-screen lg:block bg-white border-r z-30 hidden">
                 <!-- Logo -->
                 <div class="w-full h-20 flex px-4 items-center mb-16 mt-8 justify-center">
-                    <img src="img/TCL_Logo_Black.svg" alt="TCL Logo" class="w-40 h-40 mt-6 rounded-full opacity-25 hover:opacity-80 transition-opacity duration-700 ease-in-out">
+                    <img src="img/TCL_Logo_Black.svg" alt="TCL Logo"
+                        class="w-40 h-40 mt-6 rounded-full opacity-25 hover:opacity-80 transition-opacity duration-700 ease-in-out">
                 </div>
                 <!--Nav Start-->
                 <?php include 'sidebar.php'; ?>
@@ -25,29 +44,56 @@
             <?php include 'client/client_subheader.php'; ?>
 
             <!--Content Start-->
-            <div class="bg-white border border-gray-200 rounded-lg w-[90%] m-auto p-4 my-4">
-                <div class="bg-blue-500/50">Test A</div>
+            <div class="flex flex-col space-y-16 px-16">
+                <div class="tab-content" id="content-profile">Profile Content</div>
+                <div class="tab-content active" id="content-dashboard">Dashboard Content</div>
+                <div class="tab-content" id="content-settings">Settings Content</div>
+                <div class="tab-content" id="content-contacts">Contacts Content</div>
             </div>
+
+
+
+            <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                let tabs = document.querySelectorAll(".flex.flex-wrap > li");
+                let contents = document.querySelectorAll(".tab-content");
+
+                tabs.forEach((tab, index) => {
+                    tab.addEventListener("click", function() {
+                        // Hide all content areas
+                        contents.forEach(content => {
+                            content.classList.remove("active");
+                        });
+
+                        // Construct the ID for the content section based on the tab's text content
+                        let targetId =
+                            `content-${tab.textContent.trim().toLowerCase().replace(/ /g, '-')}`;
+                        let targetContent = document.getElementById(targetId);
+
+                        if (targetContent) {
+                            // Show the clicked tab's content
+                            targetContent.classList.add("active");
+                        } else {
+                            console.warn(`Element with ID ${targetId} not found.`);
+                        }
+
+                        // Remove active styles from all tabs
+                        tabs.forEach(innerTab => {
+                            innerTab.querySelector("a").classList.remove("active");
+                        });
+
+                        // Add active styles to clicked tab
+                        this.querySelector("a").classList.add("active");
+                    });
+                });
+            });
+            </script>
         </div>
     </div>
     </div>
 
-
     <!--Script Start-->
-    <script src=" https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.js">
-    </script>
-    <!-- ApexCharts JS -->
-    <script src="assets/apex_charts/apexcharts.js"></script>
-    <!-- Loading Animation -->
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            document.getElementById("progressBar").style.width = "29%";
-        });
-    </script>
-    <!-- Loading End -->
-
+    <script src=" https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.js"></script>
     <!--Custom-->
-    <script src="js/popover.js"></script>
-    <script src="js/carousel.js"></script>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
 </body>
--- dist/client/client_header.php
+++ dist/client/client_header.php
@@ -1,41 +1,48 @@
-<div class="flex flex-col mb-8 bg-slate-800 p-10">
+<div class="flex flex-col mb-5 bg-slate-800 p-5">
     <?php include 'breadcrumbs.php'; ?>
 
     <!-- Logo, dropdown, and details -->
-    <div class="flex items-center mb-8 mt-8">
-        <!-- Added items-center here -->
-
+    <div class="flex items-center mb-3 mt-3">
         <!-- Logo and Dropdown -->
-        <div class="flex flex-col items-center pr-20 my-20">
+        <div class="flex flex-col items-center pr-8 my-8 w-1/2">
             <!-- Logo -->
-            <img class="h-40 px-20 mb-4" src="img/logos/ww_white.svg" alt="Logo" />
+            <img class="h-40 px-8 mb-1" src="img/logos/ww_white.svg" alt="Logo" />
 
             <!-- Dropdown -->
             <?php include 'client_dropdown.php'; ?>
         </div>
 
         <!-- Details -->
-        <div>
-            <h1 class="mb-4 text-6xl font-extrabold text-white">Altoona</h1>
-            <div class="p-1 mb-2 text-sm font-medium rounded-full text-white border-2 border-white border-opacity-50">
+        <div class="w-1/2">
+            <h1 class="mb-2 text-6xl font-extrabold text-white">Altoona</h1>
+
+            <div
+                class="p-1 mb-1 w-2/3 text-sm font-medium rounded-full text-slate-800 bg-white border border-white border-opacity-50 text-center">
                 Full Service Customer
             </div>
-            <div class="flex items-center mb-2 text-white">
+
+            <div class="flex items-center mb-1 text-white">
                 <i class="las la-map-marker-alt text-lg pr-2"></i>
                 123 Fake Street, Altoona, PA 12345
             </div>
-            <div class="flex items-center mb-2 text-white">
+
+            <div class="flex items-center mb-1 text-white">
                 <i class="las la-phone text-lg pr-2"></i>
                 123-123-1234
             </div>
-            <div class="p-1 mb-2 text-sm font-medium rounded-full text-white border-2 border-white border-opacity-50">
+
+            <div
+                class="p-1 mb-1 w-2/3 text-sm font-medium rounded-full text-white border border-white border-opacity-50 text-center">
                 Owners
             </div>
-            <ul class="w-full justify-start flex flex-row text-white gap-4">
-                <li>Client 1</li>
-                <li>Client 2</li>
-                <li>Client 3</li>
-            </ul>
+
+            <div class="w-2/3">
+                <ul class="flex justify-between text-white gap-4">
+                    <li><i class='bx bxs-user-circle text-white text-lg mr-2'></i>Client 1</li>
+                    <li><i class='bx bxs-user-circle text-white text-lg mr-2'></i>Client 2</li>
+                    <li><i class='bx bxs-user-circle text-white text-lg mr-2'></i>Client 3</li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
