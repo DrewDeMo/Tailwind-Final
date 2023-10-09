4bb3e6c417c9a0a0515f52d42e38e9e49f6b35fb
DrewDeMo <drdemaiolo@gmail.com>
2023-10-09 19:20:50 -0400

    Update client.php

--- dist/client.php
+++ dist/client.php
@@ -12,11 +12,9 @@
 
             <!--Mast Header Background -->
             <div id="clientHeaderBG" class="bg-ww-navy-950">
-                <div id="main-nav"
-                    class="w-1/2 md:w-1/3 lg:w-64 fixed md:top-0 md:left-0 h-screen lg:block bg-white border-r z-30 hidden">
+                <div id="main-nav" class="w-1/2 md:w-1/3 lg:w-64 fixed md:top-0 md:left-0 h-screen lg:block bg-white border-r z-30 hidden">
                     <div class="w-full h-20 flex px-4 items-center mb-16 mt-8 justify-center">
-                        <img src="img/TCL_Logo_Black.svg" alt="TCL Logo"
-                            class="w-40 h-40 mt-6 rounded-full opacity-25 hover:opacity-80 transition-opacity duration-700 ease-in-out">
+                        <img src="img/TCL_Logo_Black.svg" alt="TCL Logo" class="w-40 h-40 mt-6 rounded-full opacity-25 hover:opacity-80 transition-opacity duration-700 ease-in-out">
                     </div>
 
 
@@ -37,31 +35,19 @@
     </div>
 
     <!--Preload Colors-->
-    <div class="hidden">
-        <div class="bg-rwu-red-50">a</div>
-        <div class="bg-rwu-red-100">a</div>
-        <div class="bg-rwu-red-200">a</div>
-        <div class="bg-rwu-red-300">a</div>
-        <div class="bg-rwu-red-400">a</div>
-        <div class="bg-rwu-red-500">a</div>
-        <div class="bg-rwu-red-600">a</div>
-        <div class="bg-rwu-red-700">a</div>
-        <div class="bg-rwu-red-800">a</div>
-        <div class="bg-rwu-red-900">a</div>
-        <div class="bg-rwu-red-950">a</div>
+    <div class="">
+        <div class="bg-rwu-theme-50">a</div>
+        <div class="bg-rwu-theme-100">a</div>
+        <div class="bg-rwu-theme-200">a</div>
+        <div class="bg-rwu-theme-300">a</div>
+        <div class="bg-rwu-theme-400">a</div>
+        <div class="bg-rwu-theme-500">a</div>
+        <div class="bg-rwu-theme-600">a</div>
+        <div class="bg-rwu-theme-700">a</div>
+        <div class="bg-rwu-theme-800">a</div>
+        <div class="bg-rwu-theme-900">a</div>
+        <div class="bg-rwu-theme-950">a</div>
         <!-- ... all other rwu-red classes ... -->
-        <div class="bg-rwu-blue-50">a</div>
-        <div class="bg-rwu-blue-100">a</div>
-        <div class="bg-rwu-blue-200">a</div>
-        <div class="bg-rwu-blue-300">a</div>
-        <div class="bg-rwu-blue-400">a</div>
-        <div class="bg-rwu-blue-500">a</div>
-        <div class="bg-rwu-blue-600">a</div>
-        <div class="bg-rwu-blue-700">a</div>
-        <div class="bg-rwu-blue-800">a</div>
-        <div class="bg-rwu-blue-900">a</div>
-        <div class="bg-rwu-blue-950">a</div>
-        <!-- ... all other rwu-blue classes ... -->
         <div class="bg-ww-blue-50">a</div>
         <div class="bg-ww-blue-100">a</div>
         <div class="bg-ww-blue-200">a</div>
@@ -74,18 +60,7 @@
         <div class="bg-ww-blue-900">a</div>
         <div class="bg-ww-blue-950">a</div>
         <!-- ... all other ww-blue classes ... -->
-        <div class="bg-ww-navy-50">a</div>
-        <div class="bg-ww-navy-100">a</div>
-        <div class="bg-ww-navy-200">a</div>
-        <div class="bg-ww-navy-300">a</div>
-        <div class="bg-ww-navy-400">a</div>
-        <div class="bg-ww-navy-500">a</div>
-        <div class="bg-ww-navy-600">a</div>
-        <div class="bg-ww-navy-700">a</div>
-        <div class="bg-ww-navy-800">a</div>
-        <div class="bg-ww-navy-900">a</div>
-        <div class="bg-ww-navy-950">a</div>
-        <!-- ... all other ww-navy classes ... -->
+
     </div>
     <!--End Preload Colors-->
 
