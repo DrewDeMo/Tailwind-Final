be62e5be0f43b06e9a562b81c19ab8714a4f4039
DrewDeMo <drdemaiolo@gmail.com>
2023-10-09 19:42:29 -0400

    a
    a

--- dist/client/client_owners.php
+++ dist/client/client_owners.php
@@ -1,10 +1,12 @@
 <!-- Owner Operator Information -->
 <section class="relative rounded-lg border w-full bg-white border-gray-300 overflow-hidden">
     <!-- Background Image and Overlay -->
-    <div class="relative top-0 left-0 right-0 h-16 bg-cover bg-center z-0" style="background-image: url('img/photos/clients.webp');">
-        <div class="absolute inset-0 bg-ww-navy-700 opacity-90"></div>
+    <div class="relative top-0 left-0 right-0 h-16 bg-cover bg-center z-0"
+        style="background-image: url('img/photos/clients.webp');">
+        <div class="absolute inset-0 bg-ww-blue-700 opacity-90"></div>
         <!-- Headline -->
-        <h2 class="px-4 drop-shadow-md absolute inset-x-0 bottom-0 h-full flex items-center text-2xl font-bold text-gray-100 shadow-md z-10">
+        <h2
+            class="px-4 drop-shadow-md absolute inset-x-0 bottom-0 h-full flex items-center text-2xl font-bold text-gray-100 shadow-md z-10">
             Points of Contact
         </h2>
     </div>
@@ -19,13 +21,16 @@
                 <div class="h-12 w-12 bg-ww-blue-50 rounded-lg mr-2 flex items-center justify-center">
                     <p class="text-lg text-ww-blue-300 font-bold">BH</i>
                 </div>
-                <h3 class="font-medium ml-4">Benjamin Hartley</h3><span class="bg-ww-blue-100 text-ww-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded ">Owner</span>
+                <h3 class="font-medium ml-4">Benjamin Hartley</h3><span
+                    class="bg-ww-blue-100 text-ww-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded ">Owner</span>
             </div>
 
             <div class="flex space-x-2">
                 <!-- Two buttons grouped on the right -->
-                <button><i class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
-                <button><i class="las la-envelope text-white font-lg bg-ww-blue-600 hover:bg-ww-blue-700 p-1.5 rounded-lg"></i></button>
+                <button><i
+                        class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
+                <button><i
+                        class="las la-envelope text-white font-lg bg-ww-blue-600 hover:bg-ww-blue-700 p-1.5 rounded-lg"></i></button>
             </div>
 
         </div>
@@ -40,13 +45,16 @@
                 <div class="h-12 w-12 bg-ww-blue-100 rounded-lg mr-2 flex items-center justify-center">
                     <p class="text-lg text-ww-blue-300 font-bold">CJ</p>
                 </div>
-                <h3 class="font-medium ml-4">Clara Jeffries</h3><span class="bg-ww-blue-100 text-ww-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
+                <h3 class="font-medium ml-4">Clara Jeffries</h3><span
+                    class="bg-ww-blue-100 text-ww-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
             </div>
 
             <div class="flex space-x-2">
                 <!-- Two buttons grouped on the right -->
-                <button><i class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
-                <button><i class="las la-envelope text-white font-lg bg-ww-blue-600 hover:bg-ww-blue-700 p-1.5 rounded-lg"></i></button>
+                <button><i
+                        class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
+                <button><i
+                        class="las la-envelope text-white font-lg bg-ww-blue-600 hover:bg-ww-blue-700 p-1.5 rounded-lg"></i></button>
             </div>
 
         </div>
@@ -62,13 +70,16 @@
                 <div class="h-12 w-12 bg-ww-blue-100 rounded-lg mr-2 flex items-center justify-center">
                     <p class="text-lg text-ww-blue-300 font-bold">TW</i>
                 </div>
-                <h3 class="font-medium ml-4">Thomas Whitfield</h3><span class="bg-ww-blue-100 text-ww-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded ">Owner</span>
+                <h3 class="font-medium ml-4">Thomas Whitfield</h3><span
+                    class="bg-ww-blue-100 text-ww-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded ">Owner</span>
             </div>
 
             <div class="flex space-x-2">
                 <!-- Two buttons grouped on the right -->
-                <button><i class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
-                <button><i class="las la-envelope text-white font-lg bg-ww-blue-600 hover:bg-ww-blue-700 p-1.5 rounded-lg"></i></button>
+                <button><i
+                        class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
+                <button><i
+                        class="las la-envelope text-white font-lg bg-ww-blue-600 hover:bg-ww-blue-700 p-1.5 rounded-lg"></i></button>
             </div>
 
         </div>
@@ -77,7 +88,8 @@
         <!-- Adjusted the padding to p-4 -->
         <div class="w-full flex justify-end items-center mt-4 mb-auto mr-4">
             <!-- Adjusted the right margin to mr-4 -->
-            <button type="button" class=" px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-ww-blue-600 rounded-lg hover:bg-ww-blue-700 focus:ring-4 focus:outline-none focus:ring-ww-blue-300">
+            <button type="button"
+                class=" px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-ww-blue-600 rounded-lg hover:bg-ww-blue-700 focus:ring-4 focus:outline-none focus:ring-ww-blue-300">
                 <i class="las la-mail-bulk text-xl mr-2"></i>
                 Mail All
             </button>
