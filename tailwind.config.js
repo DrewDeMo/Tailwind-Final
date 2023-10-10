81cf29498b3ca281962ad1067eb609375344094c
DrewDeMo <drdemaiolo@gmail.com>
2023-10-09 21:20:17 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,24 @@ module.exports = {
   content: ['./dist/**/*.{css,php,html,js}'],
   options: {
     safelist: [
-      'rwu-red-50', 'rwu-red-100', 'rwu-red-200', 'rwu-red-300', 'rwu-red-400', 'rwu-red-500', 'rwu-red-600', 'rwu-red-700', 'rwu-red-800', 'rwu-red-900', 'rwu-red-950',
-      'rwu-blue-50', 'rwu-blue-100', 'rwu-blue-200', 'rwu-blue-300', 'rwu-blue-400', 'rwu-blue-500', 'rwu-blue-600', 'rwu-blue-700', 'rwu-blue-800', 'rwu-blue-900', 'rwu-blue-950',
-      'ww-blue-50', 'ww-blue-100', 'ww-blue-200', 'ww-blue-300', 'ww-blue-400', 'ww-blue-500', 'ww-blue-600', 'ww-blue-700', 'ww-blue-800', 'ww-blue-900', 'ww-blue-950',
-      'ww-navy-50', 'ww-navy-100', 'ww-navy-200', 'ww-navy-300', 'ww-navy-400', 'ww-navy-500', 'ww-navy-600', 'ww-navy-700', 'ww-navy-800', 'ww-navy-900', 'ww-navy-950',
+      // ... (same as your provided list)
     ],
   },
   theme: {
     extend: {
       colors: {
         'rwu-theme': {
-          '50:'  '#FEF6FA',
-          '100:' '#F9D7E8',
-          '200:' '#F199BA',
-          '300:' '#E85A83',
-          '400:' '#DC1F44',
-          '500:' '#9D1624',
-          '600:' '#791112',
-          '700:' '#55120C',
-          '800:' '#320E07',
-          '900:' '#0E0502',
-          '950:' '#f3f4f6',
+          '50': '#FEF6FA',
+          '100': '#F9D7E8',
+          '200': '#F199BA',
+          '300': '#E85A83',
+          '400': '#DC1F44',
+          '500': '#9D1624',
+          '600': '#791112',
+          '700': '#55120C',
+          '800': '#320E07',
+          '900': '#0E0502',
+          '950': '#f3f4f6',
         },
         'rwu-theme-2': {
           '50': '#ECEFFE',
@@ -48,21 +45,11 @@ module.exports = {
           '600': '#10368A',
           '700': '#0D2D6E',
           '800': '#0A2353',
-          '900': '#ffffff',
+          '900': '#ffffff', // Ensure this is intentional, as it's white for a typically dark variant.
           '950': '#05132A',
         },
         'ww-blue-2': {
-          '50': '#E1F3F9',
-          '100': '#D0ECF5',
-          '200': '#AEDCEE',
-          '300': '#8DCBE7',
-          '400': '#6BB8E0',
-          '500': '#49A4D9',
-          '600': '#3092D4',
-          '700': '#287DBD',
-          '800': '#2269A4',
-          '900': '#1D558B',
-          '950': '#1A4C7E',
+          // ... (same as your provided list)
         },
       },
     },
