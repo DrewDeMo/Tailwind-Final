a072afc5193608b241cb3414a5e9cfcf1ff65f0f
DrewDeMo <drdemaiolo@gmail.com>
2023-10-09 21:05:44 -0400

    a

--- dist/client/client_header.php
+++ dist/client/client_header.php
@@ -12,22 +12,22 @@
                 <!-- Adjusted width here -->
                 <form class="w-full flex justify-center relative">
                     <label for="default-search"
-                        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-ww-blue-2-100">Search</label>
                     <div
-                        class="relative flex items-center w-full px-4 py-1 bg-white/5 border border-white/10 border-opacity-50 rounded-full text-sm font-medium text-white border-fade-in">
+                        class="relative flex items-center w-full px-4 py-1 bg-white/5 border border-white/10 border-opacity-50 rounded-full text-sm font-medium text-ww-blue-2-100 border-fade-in">
                         <div class="flex items-center justify-center w-full">
                             <!-- Logo left fade in -->
                             <img src="img/logos/ww_search_icon.svg" alt="Logo" class="w-8 h-8 mr-2 logo-fade-in" />
                             <input type="search" id="default-search"
-                                class="default-search-input block w-full p-4 pl-6  text-white border-0 bg-transparent focus:ring-transparent rounded-full"
+                                class="default-search-input block w-full p-4 pl-6  text-ww-blue-2-100 border-0 bg-transparent focus:ring-transparent rounded-full"
                                 placeholder="Window World of Altoona" required>
                             <div id="autocomplete-dropdown"
                                 class="hidden absolute mt-10 w-full bg-ww-blue-800 border border-ww-blue-700 border-opacity-50 rounded-md shadow-lg">
                             </div>
                             <button type="submit"
-                                class="flex items-center justify-center bg-ww-blue-500 duration-800 hover:bg-ww-blue-600 focus:ring-4 focus:outline-none focus:ring-ww-blue-300 font-medium rounded-full text-sm h-8 w-8 px-2 py-2 text-white ml-2">
-                                <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 20 20">
+                                class="flex items-center justify-center bg-ww-blue-500 duration-800 hover:bg-ww-blue-600 focus:ring-4 focus:outline-none focus:ring-ww-blue-300 font-medium rounded-full text-sm h-8 w-8 px-2 py-2 text-ww-blue-2-100 ml-2">
+                                <svg class="w-6 h-6 text-ww-blue-2-100" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                         stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
@@ -42,30 +42,30 @@
         <div class="w-1/2 border-opacity-50 border-white pl-8">
             <h1 class="mb-2 text-6xl font-extrabold text-ww-blue-900">Altoona</h1>
             <div
-                class="flex items-center justify-center w-2/3 px-4 py-1 text-center bg-white/5 border border-white/10 border-opacity-50 rounded-lg text-sm font-medium text-white">
-                <div class="mr-2 bg-green-500 w-2 h-2 rounded-full glow-dot"></div>
+                class="flex text-ww-blue-900 items-center justify-center w-2/3 px-4 py-1 text-center bg-white/5 border border-white/10 border-opacity-50 rounded-lg text-sm font-medium text-ww-blue-2-100">
+                <div class="mr-2  bg-green-500 w-2 h-2 rounded-full glow-dot"></div>
                 Full Service Customer
             </div>
-            <div class="flex items-center my-4 text-white">
+            <div class="flex items-center my-4 text-ww-blue-2-100">
                 <i class="las la-map-marker-alt text-lg pr-2"></i>
-                <div id="clientAddress">123 Fake Street, Altoona, PA 12345</div>
+                <div class="text-ww-blue-900" id="clientAddress">123 Fake Street, Altoona, PA 12345</div>
             </div>
-            <div class="flex items-center my-4 text-white">
+            <div class="flex items-center my-4 text-ww-blue-2-100">
                 <i class="las la-phone text-lg pr-2"></i>
-                <div id="clientPhone">123-123-1234</div>
+                <div class="text-ww-blue-900" id="clientPhone">123-123-1234</div>
             </div>
             <div
-                class="p-1 my-4 w-2/3 text-sm font-medium rounded-full text-white border border-white border-opacity-50 text-center">
+                class="p-1 my-4 w-2/3 text-sm font-medium rounded-full text-ww-blue-900 border border-ww-blue-50 border-opacity-50 text-center">
                 Owners
             </div>
             <div class="w-2/3 pb-4">
-                <ul class="flex justify-between text-white gap-4">
+                <ul class="flex justify-between text-ww-blue-900 gap-4">
                     <li class="text-sm flex items-center"><i
-                            class='las la-user-circle text-white text-lg mr-2'></i>Benjamin Hartley</li>
+                            class='las la-user-circle text-ww-blue-2-100 text-lg mr-2'></i>Benjamin Hartley</li>
                     <li class="text-sm flex items-center"><i
-                            class='las la-user-circle text-white text-lg mr-2'></i>Clara Jeffries</li>
+                            class='las la-user-circle text-ww-blue-2-100 text-lg mr-2'></i>Clara Jeffries</li>
                     <li class="text-sm flex items-center"><i
-                            class='las la-user-circle text-white text-lg mr-2'></i>Thomas Whitfield</li>
+                            class='las la-user-circle text-ww-blue-2-100 text-lg mr-2'></i>Thomas Whitfield</li>
                 </ul>
             </div>
         </div>
--- dist/client/client_owners.php
+++ dist/client/client_owners.php
@@ -19,7 +19,7 @@
                 <div class="h-12 w-12 bg-ww-blue-2-50 rounded-lg mr-2 flex items-center justify-center">
                     <p class="text-lg text-ww-blue-2-500 font-bold">BH</i>
                 </div>
-                <h3 class="font-medium ml-4">Benjamin Hartley</h3><span class="bg-ww-blue-50/40 text-ww-blue-600 text-xs font-medium mr-2 px-2.5 py-0.5 rounded ">Owner</span>
+                <h3 class="font-medium ml-4">Benjamin Hartley</h3><span class="bg-ww-blue-2-50 text-ww-blue-2-700 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
             </div>
 
             <div class="flex space-x-2">
@@ -37,10 +37,10 @@
 
             <div class="flex items-center space-x-2">
                 <!-- Grouped user icon and name -->
-                <div class="h-12 w-12 bg-ww-blue-100 rounded-lg mr-2 flex items-center justify-center">
-                    <p class="text-lg text-ww-blue-300 font-bold">CJ</p>
+                <div class="h-12 w-12 bg-ww-blue-2-50 rounded-lg mr-2 flex items-center justify-center">
+                    <p class="text-lg text-ww-blue-2-500 font-bold">CJ</p>
                 </div>
-                <h3 class="font-medium ml-4">Clara Jeffries</h3><span class="bg-ww-blue-100 text-ww-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
+                <h3 class="font-medium ml-4">Clara Jeffries</h3><span class="bg-ww-blue-2-50 text-ww-blue-2-700 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
             </div>
 
             <div class="flex space-x-2">
@@ -59,16 +59,16 @@
 
             <div class="flex items-center space-x-2">
                 <!-- Grouped user icon and name -->
-                <div class="h-12 w-12 bg-ww-blue-100 rounded-lg mr-2 flex items-center justify-center">
-                    <p class="text-lg text-ww-blue-300 font-bold">TW</i>
+                <div class="h-12 w-12 bg-ww-blue-2-50 rounded-lg mr-2 flex items-center justify-center">
+                    <p class="text-lg text-ww-blue-2-500 font-bold">TW</i>
                 </div>
-                <h3 class="font-medium ml-4">Thomas Whitfield</h3><span class="bg-ww-blue-100 text-ww-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded ">Owner</span>
+                <h3 class="font-medium ml-4">Thomas Whitfield</h3><span class="bg-ww-blue-2-50 text-ww-blue-2-700 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
             </div>
 
             <div class="flex space-x-2">
                 <!-- Two buttons grouped on the right -->
                 <button><i class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
-                <button><i class="las la-envelope text-white font-lg bg-ww-blue-600 hover:bg-ww-blue-700 p-1.5 rounded-lg"></i></button>
+                <button><i class="las la-envelope text-white font-lg bg-ww-blue-500 hover:bg-ww-blue-600 p-1.5 rounded-lg"></i></button>
             </div>
 
         </div>
@@ -77,7 +77,7 @@
         <!-- Adjusted the padding to p-4 -->
         <div class="w-full flex justify-end items-center mt-4 mb-auto mr-4">
             <!-- Adjusted the right margin to mr-4 -->
-            <button type="button" class=" px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-ww-blue-600 rounded-lg hover:bg-ww-blue-700 focus:ring-4 focus:outline-none focus:ring-ww-blue-300">
+            <button type="button" class=" px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-ww-blue-500 rounded-lg hover:bg-ww-blue-600 focus:ring-4 focus:outline-none focus:ring-ww-blue-300">
                 <i class="las la-mail-bulk text-xl mr-2"></i>
                 Mail All
             </button>
