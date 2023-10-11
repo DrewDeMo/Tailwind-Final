8f21a0f17a535469cade961e5f0e9700916b5b9a
DrewDeMo <drdemaiolo@gmail.com>
2023-10-11 15:36:25 -0400

    Update client_rwu.php

--- dist/client/client_rwu.php
+++ dist/client/client_rwu.php
@@ -4,16 +4,11 @@
             <a href="#" data-tab="clientNav-tab7" class="clientNav-tab-link clientNav-active font-medium pr-1">
                 <i class="las la-tachometer-alt text-xl mr-1" style="line-height: 1; vertical-align: bottom;"></i> RWU
             </a>
-            <a href="#" data-tab="clientNav-tab9" class="clientNav-tab-link pr-1"><i class="las las la-tag text-xl mr-1"
-                    style="line-height: 1; vertical-align: bottom;"></i>Offers</a>
-            <a href="#" data-tab="clientNav-tab8" class="clientNav-tab-link pr-1"><i
-                    class="las la-bullhorn text-xl mr-1" style="line-height: 1; vertical-align: bottom;"></i>Global
+            <a href="#" data-tab="clientNav-tab9" class="clientNav-tab-link pr-1"><i class="las las la-tag text-xl mr-1" style="line-height: 1; vertical-align: bottom;"></i>Offers</a>
+            <a href="#" data-tab="clientNav-tab8" class="clientNav-tab-link pr-1"><i class="las la-bullhorn text-xl mr-1" style="line-height: 1; vertical-align: bottom;"></i>Global
                 Revisions</a>
-            <a href="#" data-tab="clientNav-tab10" class="clientNav-tab-link pr-1"><i
-                    class="las la-bullseye text-xl mr-1"
-                    style="line-height: 1; vertical-align: bottom;"></i>Tracking</a>
-            <a href="#" data-tab="clientNav-tab11" class="clientNav-tab-link pr-1"><i class="las la-icons text-xl mr-1"
-                    style="line-height: 1; vertical-align: bottom;"></i>Credentials</a>
+            <a href="#" data-tab="clientNav-tab10" class="clientNav-tab-link pr-1"><i class="las la-bullseye text-xl mr-1" style="line-height: 1; vertical-align: bottom;"></i>Tracking</a>
+            <a href="#" data-tab="clientNav-tab11" class="clientNav-tab-link pr-1"><i class="las la-icons text-xl mr-1" style="line-height: 1; vertical-align: bottom;"></i>Credentials</a>
 
             <span class="clientNav-indicator"></span>
         </nav>
@@ -29,8 +24,7 @@
                     <div class="relative top-0 left-0 right-0 h-16 bg-cover bg-center z-0">
                         <div class="absolute inset-0 opacity-90 clientCardHeadlines"></div>
                         <!-- Headline -->
-                        <h2
-                            class="px-4 absolute inset-x-0 bottom-0 h-full flex items-center text-2xl font-bold text-gray-100 shadow-md z-10">
+                        <h2 class="px-4 absolute inset-x-0 bottom-0 h-full flex items-center text-2xl font-bold text-gray-100 shadow-md z-10">
                             Contacts
                         </h2>
                     </div>
@@ -38,24 +32,20 @@
 
                     <!--Owner 1-->
                     <div class="p-2 mt-2">
-                        <div
-                            class="flex items-center justify-between w-full hover:bg-gray-50 duration-400 rounded-lg p-2">
+                        <div class="flex items-center justify-between w-full hover:bg-gray-50 duration-400 rounded-lg p-2">
 
                             <div class="flex items-center space-x-2">
                                 <!-- Grouped user icon and name -->
-                                <div
-                                    class="h-12 w-12 bg-ww-blue-sub-50 rounded-lg mr-2 flex items-center justify-center contactCard">
-                                    <p class="text-lg  font-bold">BH</i>
+                                <div class="h-12 w-12 bg-ww-blue-sub-50 rounded-lg mr-2 flex items-center justify-center contactCard">
+                                    <p class="text-lg  font-bold">CS</i>
                                 </div>
-                                <h3 class="font-medium ml-4">Benjamin Hartley</h3><span
-                                    class="bg-ww-blue-sub-50 companyTag text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
+                                <h3 class="font-medium ml-4">Chad Shirey</h3><span class="bg-ww-blue-sub-50 companyTag text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
                             </div>
 
                             <div class="flex space-x-2">
                                 <!-- Two buttons grouped on the right -->
                                 <button><i class="las la-copy font-lg border p-1.5 rounded-lg"></i></button>
-                                <button><i
-                                        class="las la-envelope mailButton text-white font-lg bg-ww-blue-500 hover:bg-ww-blue-600 p-1.5 rounded-lg"></i></button>
+                                <button><i class="las la-envelope mailButton text-white font-lg bg-ww-blue-500 hover:bg-ww-blue-600 p-1.5 rounded-lg"></i></button>
                             </div>
 
                         </div>
@@ -63,62 +53,32 @@
 
                     <!--Owner 2-->
                     <div class="p-2">
-                        <div
-                            class="flex items-center justify-between w-full hover:bg-gray-50 duration-400 rounded-lg p-2">
+                        <div class="flex items-center justify-between w-full hover:bg-gray-50 duration-400 rounded-lg p-2">
 
                             <div class="flex items-center space-x-2">
                                 <!-- Grouped user icon and name -->
-                                <div
-                                    class="h-12 w-12 bg-ww-blue-sub-50 rounded-lg mr-2 flex items-center justify-center contactCard">
-                                    <p class="text-lg text-ww-blue-sub-500 font-bold">CJ</p>
+                                <div class="h-12 w-12 bg-ww-blue-sub-50 rounded-lg mr-2 flex items-center justify-center contactCard">
+                                    <p class="text-lg text-ww-blue-sub-500 font-bold">PB</p>
                                 </div>
-                                <h3 class="font-medium ml-4">Clara Jeffries</h3><span
-                                    class="bg-ww-blue-sub-50 companyTag text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
+                                <h3 class="font-medium ml-4">Paul Balzano</h3><span class="bg-ww-blue-sub-50 companyTag text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
                             </div>
 
                             <div class="flex space-x-2">
                                 <!-- Two buttons grouped on the right -->
-                                <button><i
-                                        class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
-                                <button><i
-                                        class="las la-envelope mailButton text-white font-lg bg-ww-blue-600 hover:bg-ww-blue-700 p-1.5 rounded-lg"></i></button>
+                                <button><i class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
+                                <button><i class="las la-envelope mailButton text-white font-lg bg-ww-blue-600 hover:bg-ww-blue-700 p-1.5 rounded-lg"></i></button>
                             </div>
 
                         </div>
                     </div>
 
 
-                    <!--Owner 3-->
-                    <div class="p-2">
-                        <div
-                            class="flex items-center justify-between w-full hover:bg-gray-50 duration-400 rounded-lg p-2">
 
-                            <div class="flex items-center space-x-2">
-                                <!-- Grouped user icon and name -->
-                                <div
-                                    class="h-12 w-12 bg-ww-blue-sub-50 rounded-lg mr-2 flex items-center justify-center contactCard">
-                                    <p class="text-lg text-ww-blue-sub-500 font-bold">TW</i>
-                                </div>
-                                <h3 class="font-medium ml-4">Thomas Whitfield</h3><span
-                                    class="bg-ww-blue-sub-50 companyTag text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Owner</span>
-                            </div>
-
-                            <div class="flex space-x-2">
-                                <!-- Two buttons grouped on the right -->
-                                <button><i
-                                        class="las la-copy text-gray-600 font-lg border border-gray-200 hover:bg-gray-100 p-1.5 rounded-lg"></i></button>
-                                <button><i
-                                        class="las la-envelope mailButton text-white font-lg bg-ww-blue-500 hover:bg-ww-blue-600 p-1.5 rounded-lg"></i></button>
-                            </div>
-
-                        </div>
-                    </div>
                     <div class="p-4 flex-grow">
                         <!-- Adjusted the padding to p-4 -->
                         <div class="w-full flex justify-end items-center mt-4 mb-auto mr-4">
                             <!-- Adjusted the right margin to mr-4 -->
-                            <button type="button"
-                                class=" px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-ww-blue-500 rounded-lg hover:bg-ww-blue-600 focus:ring-4 focus:outline-none focus:ring-ww-blue-300">
+                            <button type="button" class=" px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-ww-blue-500 rounded-lg hover:bg-ww-blue-600 focus:ring-4 focus:outline-none focus:ring-ww-blue-300">
                                 <i class="las la-mail-bulk text-xl mr-2"></i>
                                 Mail All
                             </button>
@@ -133,8 +93,7 @@
                     <div class="relative top-0 left-0 right-0 h-16 bg-ww-blue-950 bg-cover bg-center z-0">
                         <div class="absolute inset-0 bg-ww-navy-700 opacity-90 clientCardHeadlines"></div>
                         <!-- Headline -->
-                        <h2
-                            class="px-4 absolute inset-x-0 bottom-0 h-full flex items-center text-2xl font-bold text-gray-100 shadow-md z-10">
+                        <h2 class="px-4 absolute inset-x-0 bottom-0 h-full flex items-center text-2xl font-bold text-gray-100 shadow-md z-10">
                             Resources
                         </h2>
                     </div>
@@ -143,8 +102,7 @@
 
                     <!--Item 1-->
                     <div class="p-2 mt-2">
-                        <div
-                            class="flex items-center justify-between w-full hover:bg-gray-50 duration-400 rounded-lg p-2">
+                        <div class="flex items-center justify-between w-full hover:bg-gray-50 duration-400 rounded-lg p-2">
 
                             <div class="flex items-center space-x-2">
                                 <!-- Grouped user icon and name -->
@@ -153,8 +111,7 @@
 
                             <div class="flex space-x-2">
                                 <!-- Two buttons grouped on the right -->
-                                <div
-                                    class="h-16 w-10 px-1 bg-green-400 rounded-lg mr-2 flex items-center justify-center relative ">
+                                <div class="h-16 w-10 px-1 bg-green-400 rounded-lg mr-2 flex items-center justify-center relative ">
                                     <p class=" font-medium p-1 transition-opacity duration-300 hover:opacity-0">
                                         <i class="lab la-google-drive text-white text-xl"></i>
                                     </p>
@@ -165,8 +122,7 @@
                                     .SVG</p>
 
                                 </div>
-                                <div
-                                    class="h-16 w-10 px-1 bg-gray-100 rounded-lg mr-2 flex items-center justify-center">
+                                <div class="h-16 w-10 px-1 bg-gray-100 rounded-lg mr-2 flex items-center justify-center">
                                     <p class="text-xs text-gray-700 font-medium p-1">.PNG</i>
                                 </div>
                             </div>
@@ -176,18 +132,15 @@
 
                     <!--Item 2-->
                     <div class="p-2 mt-2">
-                        <div
-                            class="flex items-center justify-between w-full hover:bg-gray-50 duration-400 rounded-lg p-2">
+                        <div class="flex items-center justify-between w-full hover:bg-gray-50 duration-400 rounded-lg p-2">
 
                             <!-- Text all the way on the left -->
                             <h3 class="font-light italic text-gray-300">No Owner Images</h3>
 
                             <!-- No image card all the way on the right -->
                             <div class="flex items-center space-x-2">
-                                <div
-                                    class="h-16 w-10 px-1 border border-gray-100 rounded-lg flex items-center justify-center">
-                                    <p class="text-xl text-gray-200 font-medium text-center p-2"><i
-                                            class="las la-user-alt-slash"></i>
+                                <div class="h-16 w-10 px-1 border border-gray-100 rounded-lg flex items-center justify-center">
+                                    <p class="text-xl text-gray-200 font-medium text-center p-2"><i class="las la-user-alt-slash"></i>
                                     </p>
                                 </div>
                             </div>
@@ -205,33 +158,28 @@
                     <h2 class="text-lg font-medium">Landing Pages</h2>
 
                     <div class="flex flex-row gap-2 mt-4 mb-1.5">
-                        <span
-                            class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded flex items-center justify-center">PPC</span>
+                        <span class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded flex items-center justify-center">PPC</span>
                         <p>Branded</p>
                     </div>
 
                     <div class="flex flex-row gap-2 mt-4 mb-1.5">
-                        <span
-                            class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded flex items-center justify-center">PPC</span>
+                        <span class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded flex items-center justify-center">PPC</span>
                         <p>Non-Branded</p>
                     </div>
 
                     <div class="flex flex-row gap-2 mt-4 mb-1.5">
-                        <span
-                            class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded flex items-center justify-center">PPC</span>
+                        <span class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded flex items-center justify-center">PPC</span>
                         <p>Competitor Branded</p>
                     </div>
 
                     <div class="flex flex-row gap-2 mt-4 mb-1.5">
-                        <div
-                            class="bg-blue-100 text-blue-600 p-1 px-2.5 rounded-lg font-medium w-fit text-xs flex items-center justify-center">
+                        <div class="bg-blue-100 text-blue-600 p-1 px-2.5 rounded-lg font-medium w-fit text-xs flex items-center justify-center">
                             FB</div>
                         <p>Windows</p>
                     </div>
 
                     <div class="flex flex-row gap-2 mt-4 mb-1.5">
-                        <div
-                            class="bg-blue-100 text-blue-600 p-1 px-2.5 rounded-lg font-medium w-fit text-xs flex items-center justify-center">
+                        <div class="bg-blue-100 text-blue-600 p-1 px-2.5 rounded-lg font-medium w-fit text-xs flex items-center justify-center">
                             FB</div>
                         <p>Siding & Doors</p>
                     </div>
@@ -276,13 +224,10 @@
                 </div>
 
                 <!--Card 3 GIS Map-->
-                <div
-                    class="max-w-md mx-auto bg-white rounded-xl border border-gray-200 rounded-lg overflow-hidden md:max-w-2xl">
+                <div class="max-w-md mx-auto bg-white rounded-xl border border-gray-200 rounded-lg overflow-hidden md:max-w-2xl">
                     <div class="relative">
                         <!-- Mapbox Embed -->
-                        <iframe width="100%"
-                            src="https://api.mapbox.com/styles/v1/tclmapping/cldbyt0nb000801qotp68l5d7.html?title=false&amp;access_token=pk.eyJ1IjoidGNsbWFwcGluZyIsImEiOiJjbDF3YnN1NzgyN3ltM2psbTVxdWNhcjRtIn0.xq9gme4kjVJB1iFpM89x6w&amp;zoomwheel=falsetrue&amp;zoom=true#6.75/40.651/-78.178"
-                            title="RWU Comp Projects" style="border: none;"></iframe>
+                        <iframe width="100%" src="https://api.mapbox.com/styles/v1/tclmapping/cldbyt0nb000801qotp68l5d7.html?title=false&amp;access_token=pk.eyJ1IjoidGNsbWFwcGluZyIsImEiOiJjbDF3YnN1NzgyN3ltM2psbTVxdWNhcjRtIn0.xq9gme4kjVJB1iFpM89x6w&amp;zoomwheel=falsetrue&amp;zoom=true#6.75/40.651/-78.178" title="RWU Comp Projects" style="border: none;"></iframe>
                     </div>
                     <div class="p-4">
                         <div class="tracking-wide text-lg font-medium">Store Location</div>
@@ -291,8 +236,7 @@
                             1000 Plank Road, Duncansville, PA 16635
                         </p>
                         <div class="tracking-wide text-lg font-medium mt-4 mb-1">Counties Served</div>
-                        <div
-                            class="bg-gray-50 justify-between text-gray-500 text-sm p-1.5 border border-gray-100 rounded-lg">
+                        <div class="bg-gray-50 justify-between text-gray-500 text-sm p-1.5 border border-gray-100 rounded-lg">
                             <ul class="flex justify-between gap-2 pr-8 p-2 font-medium">
                                 <li>Bedford^</li>
                                 <li>Blair</li>
